8365d136c89593a8ab81e701bec3ce9f19b799c6
srm-scm-solutions <scm_bu@srmtech.com>
2024-06-18 00:54:29 -0400

    added tabular output view, cpu information
    Per Ashwin's request, added a tabuluar output view for the labor plan model. Feature added,
    1. Allowing user to select a date range for the model (select a specific week to plan)
    2. Compute CPU
    3. Display labor headcount and hours need in table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime,timedelta,date
 
 st.set_page_config(
-    page_title="Labor Planning Model",
+    page_title="Labor Planning Application",
     page_icon=":brain:",
     layout="wide"
 )
@@ -22,6 +22,7 @@ productivity_template_str = productivity_template.to_csv(index=False)
 # data input
 
 shift_hrs=st.text_input('Shift hours per day','8')
+labor_rate=st.text_input('Labor rate per hour in USD','20.5')
 
 st.sidebar.header("Please input your data here")
 st.sidebar.subheader("Forecasts")
@@ -31,6 +32,7 @@ forecast_file=st.sidebar.file_uploader("Upload forecast file")
 st.sidebar.subheader("Process rates")
 st.sidebar.download_button(label="Click to download a rate template file",data=productivity_template_str,file_name='process_rate_template.csv',mime='text/csv')
 rate_file=st.sidebar.file_uploader("Upload process rate file")
+
 
 if forecast_file is None:
     st.info(" Upload a forecast file through config")
@@ -43,17 +45,36 @@ if rate_file is None:
 
 df_forecast=pd.read_csv(forecast_file,skiprows=1,names=['business_unit1','business_unit2','date','process_1','forecast'])
 df_rate=pd.read_csv(rate_file,skiprows=1,names=['business_unit1','business_unit2','process_1','process_2','unit_rate'])
-
-#df_forecast=pd.read_csv("r'C:\Users\mistryn\Documents\streamlit_laborplan-main\input_files\\"forecast_input_file.csv',skiprows=1,names=['business_unit1','business_unit2','date','process_1','forecast'])
-#df_rate=pd.read_csv(r'C:\Users\mistryn\Documents\streamlit_laborplan-main\input_files\\process_rate_template.csv',skiprows=1,names=['business_unit1','business_unit2','process_1','process_2','unit_rate'])
-
-
 df_forecast['date']=pd.to_datetime(df_forecast['date']).dt.date
+
+min_date=df_forecast['date'].min()
+max_date=df_forecast['date'].max()
+
+
+st.title("Select date range")
+col1,col2=st.columns(2)
+with col1:
+    start_date=st.date_input("Pick start date",value=min_date,min_value=min_date,max_value=max_date)
+
+with col2:
+    end_date=st.date_input("Pick end date",value=max_date,min_value=min_date,max_value=max_date)
+    if end_date<=start_date:
+        st.warning('Please select a end date that is after start date',icon="⚠️")
+        st.stop()
+
+st.sidebar.subheader("Process rates")
+
+
+df_forecast=df_forecast[(df_forecast['date']>=start_date) & (df_forecast['date']<=end_date)]
 df_forecast[['business_unit1','business_unit2','process_1']]=df_forecast[['business_unit1','business_unit2','process_1']].apply(lambda x:x.str.upper())
 df_rate[['business_unit2','process_1','process_2']]=df_rate[['business_unit2','process_1','process_2']].apply(lambda x:x.str.upper())
 
 df_forecast.head()
 df_rate.head()
+
+st.sidebar.subheader("Process rates")
+
+
 
 st.subheader("Data view")
 col1,col2=st.columns(2)
@@ -81,7 +102,8 @@ if button_result==True:
     df_plan=calculate_hours(df_forecast,df_rate)
 
 
-    df_plan['labor_hours']=df_plan['forecast'] / df_plan['unit_rate']
+    df_plan['labor_hours']=np.ceil(df_plan['forecast'] / df_plan['unit_rate'])
+    df_plan['labor_cost']=df_plan['labor_hours']*float(labor_rate)
     df_plan['headcount']=df_plan['labor_hours'] / float(shift_hrs)
     df_plan['headcount']=np.ceil(df_plan['headcount'])
     df_inbound=df_plan[df_plan['process_1'].isin(['inbound'])]
@@ -92,21 +114,41 @@ if button_result==True:
 
     df=df_plan[df_plan['date']>=start_date1]
     df['week']=df['date'].apply(lambda x:pd.to_datetime(x)).dt.strftime('%Y-%U')
-    df1=df.groupby(by=['week','process_1'],as_index=False).agg({'labor_hours':'sum',
-                                                                    'headcount':'mean'})
-    df1[['headcount','labor_hours']]=df1[['headcount','labor_hours']].apply(lambda x:np.ceil(x))
 
-    st.subheader("Weekly hours by process")
-    st.bar_chart(df1,x='week',y='labor_hours',color='process_1')
+    st.write('Model run is complete! Following is the output summary')
 
-    st.subheader("Weekly headcount by process")
-    st.bar_chart(df1,x='week',y='headcount',color='process_1')
+    df1=df.groupby(by=['week','process_1'],as_index=False).agg({'labor_cost':'sum','labor_hours':'sum','forecast':'sum'})
+    df1['weekly_headcount']=df1['labor_hours']/40
+    
+    df1[['weekly_headcount','labor_hours','labor_cost']]=df1[['weekly_headcount','labor_hours','labor_cost']].apply(lambda x:np.ceil(x).fillna(0)).astype(int)
+
+    total_cost=df1['labor_cost'].sum()
+    total_units=df1['forecast'].sum()
+    total_cpu=np.round(total_cost/total_units,2)
+
+    df2=pd.pivot_table(df1,values=['weekly_headcount','labor_cost'],index=['process_1'],columns=['week'],aggfunc=np.sum)
+    df2.loc['TOTAL']=df2.sum(axis=0)
+
+    df3=pd.pivot_table(df,values='labor_hours',index=['process_1'],columns=['date'],aggfunc=np.sum)
+    df3.loc['TOTAL']=df3.sum(axis=0)
+
+    st.write('Weekly headcount by process')
+    st.table(data=df2)
+
+    st.write('Daily hours by process')
+    st.table(data=df3)
+
+
+    st.write('Total cost per unit is USD',total_cpu)
+
+   
+
 
 def convert_df(df):
-   return df.to_csv(index=False).encode('utf-8')
+    return df.to_csv(index=False).encode('utf-8')
 
 csv=convert_df(df1)
 
 st.download_button(label="Download model results",data=csv,file_name='labor plan output.csv',mime='text/csv')
 
-st.write("Thank you for visiting our model today! Have a nice day")
+st.write("Thank you for visiting our app today! Have a nice day")
